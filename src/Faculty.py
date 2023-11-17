1f1d784be5aa25d8c1ac20a72a45b6856bbed008
Arjen Catherine <144413569+ACPMolina@users.noreply.github.com>
2023-11-17 19:39:18 +0800

    Update Faculty.py

--- src/Faculty.py
+++ src/Faculty.py
@@ -1,24 +1,16 @@
 from User import User
+from BaseModel import BaseModel
+from peewee import *
+
+db = SqliteDatabase('studentdatabase.db') 
+
 class Faculty(User):
-    def __init__(self, email, password, fname, mname, lname, facultyID):
-        super().__init__(email, password, fname, mname, lname)
-        self.facultyID = facultyID
-        self.assignedSections = []
-
-    def getFacultyID(self):
-        return self.facultyID
-
-    def getAssignedSections(self):
-        return self.assignedSections
-
-    def setAssignedSection(self, newSectionID):
-        self.assignedSections.append(newSectionID)
-
-    def getClassName(self):
-        return "Faculty"
-
-    def setStudentGrade(self, student, sectionID, newGrade):
-        student.grades[sectionID] = newGrade
-
-    def __eq__(self, compare):
-        return self.facultyID == compare.facultyID
+    facultyID = AutoField()
+    userID = ForeignKeyField(User, to_field="userID")
+    fname = CharField()
+    mname = CharField()
+    lname = CharField()
+    email = CharField(unique = True)
+    password = CharField()
+    role = CharField()
+    assignedSections = CharField()
