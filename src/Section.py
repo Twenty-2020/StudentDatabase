bb8a2e99b7d25a825ae18766e6e67acdc1e5f56e
Arjen Catherine <144413569+ACPMolina@users.noreply.github.com>
2023-11-17 19:42:16 +0800

    Update Section.py

--- src/Section.py
+++ src/Section.py
@@ -1,40 +1,8 @@
-class Section:
-    def __init__(self, courseID, sectionID, sectionName, assignedFacultyID):
-        self.courseID = courseID
-        self.sectionID = sectionID
-        self.sectionName = sectionName
-        self.assignedFacultyID = assignedFacultyID
-        self.enrolledStudentIDs = []
+from BaseModel import BaseModel
+from peewee import *
 
-    # GETTER METHODS
-    def getCourseID(self):
-        return self.courseID
+db = SqliteDatabase('studentdatabase.db') 
 
-    def getSectionID(self):
-        return self.sectionID
-
-    def getSectionName(self):
-        return self.sectionName
-
-    def getAssignedFacultyID(self):
-        return self.assignedFacultyID
-
-    def getEnrolledStudentIDs(self):
-        return self.enrolledStudentIDs
-
-    # SETTER METHODS
-    def setSectionName(self, newSectionName):
-        self.sectionName = newSectionName
-
-    def setAssignedFaculty(self, newFacultyID):
-        self.assignedFacultyID = newFacultyID
-
-    def addEnrolledStudent(self, studentID):
-        self.enrolledStudentIDs.append(studentID)
-
-    def removeEnrolledStudent(self, studentID):
-        self.enrolledStudentIDs.remove(studentID)
-
-    def __eq__(self, compare):
-        return self.sectionID == compare.sectionID
-
+class Section(BaseModel):
+    sectionID = AutoField()
+    sectionname = CharField()
