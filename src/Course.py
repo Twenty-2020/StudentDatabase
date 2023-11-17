d85d5b26bf31667d5d099357eb4f4c392bfe07a6
Arjen Catherine <144413569+ACPMolina@users.noreply.github.com>
2023-11-17 19:40:57 +0800

    Update Course.py

--- src/Course.py
+++ src/Course.py
@@ -1,23 +1,8 @@
-class Course:
-    def __init__(self, courseID, courseName):
-        self.courseID = courseID
-        self.courseName = courseName
-        self.sectionIDs = []
+from BaseModel import BaseModel
+from peewee import *
 
-    def getCourseID(self):
-        return self.courseID
+db = SqliteDatabase('studentdatabase.db') 
 
-    def getCourseName(self):
-        return self.courseName
-
-    def getSectionIDs(self):
-        return self.sectionIDs
-
-    def setCourseName(self, newCourseName):
-        self.courseName = newCourseName
-
-    def addSection(self, sectionID):
-        self.sectionIDs.append(sectionID)
-
-    def __eq__(self, compare):
-        return self.courseID == compare.courseID
+class Course(BaseModel):
+    courseID = AutoField()
+    coursename = CharField()
