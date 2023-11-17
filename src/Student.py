4ce2215e2d75f0301c1ad76260775285d2f6cf3a
Arjen Catherine <144413569+ACPMolina@users.noreply.github.com>
2023-11-17 19:42:57 +0800

    Update Student.py

--- src/Student.py
+++ src/Student.py
@@ -1,35 +1,19 @@
 from User import User
+from BaseModel import BaseModel
+from peewee import *
+
+db = SqliteDatabase('studentdatabase.db') 
+
 class Student(User):
-    def __init__(self, email, password, fname, mname, lname, studentID, uploadedDocuments=[]):
-        super().__init__(email, password, fname, mname, lname)
-        self.studentID = studentID
-        self.uploadedDocuments = uploadedDocuments
-        self.enrolledSections = []
-        self.grades = {}
+    studentID = AutoField()
+    userID = ForeignKeyField(User, to_field="userID")
+    fname = CharField()
+    mname = CharField()
+    lname = CharField()
+    email = CharField(unique = True)
+    password = CharField()
+    role = CharField()
+    uploadeddocs = CharField()
+    grades = IntegerField()
+    enrolledSections = CharField()
 
-    def getStudentID(self):
-        return self.studentID
-
-    def getEnrolledSections(self):
-        return self.enrolledSections
-
-    def getUploadedDocuments(self):
-        return self.uploadedDocuments
-
-    def getGrade(self, sectionID):
-        if sectionID in self.grades:
-            return self.grades[sectionID]
-        else:
-            return 0.0
-
-    def getClassName(self):
-        return "Student"
-
-    def setEnrolledSection(self, newSectionID):
-        self.enrolledSections.append(newSectionID)
-
-    def setUploadedDocument(self, newDocumentID):
-        self.uploadedDocuments.append(newDocumentID)
-
-    def __eq__(self, compare):
-        return self.studentID == compare.studentID
