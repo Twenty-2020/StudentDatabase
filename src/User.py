71f84b90cd7c031488f83c4d12dc0429a55a23ca
Arjen Catherine <144413569+ACPMolina@users.noreply.github.com>
2023-11-18 17:01:57 +0800

    Update User.py

--- src/User.py
+++ src/User.py
@@ -4,10 +4,10 @@ from BaseModel import BaseModel
 db = SqliteDatabase('studentdatabase.db') 
 
 class User(BaseModel):
-    userID = AutoField()
-    fname = CharField()
+    userID = AutoField(null = False)
+    fname = CharField(null = False)
     mname = CharField()
-    lname = CharField()
+    lname = CharField(null = False)
     email = CharField(unique = True)
     password = CharField()
     role = CharField()
