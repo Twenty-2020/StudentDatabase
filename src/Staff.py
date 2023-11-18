26f2b7823b05efb18e39226775e01c685799e857
Aciel <106684053+Aciel07@users.noreply.github.com>
2023-11-18 22:06:13 +0800

    Update Staff.py

--- src/Staff.py
+++ src/Staff.py
@@ -1,15 +1,16 @@
 from User import User
 from BaseModel import BaseModel
-from peewee import *
-
-db = SqliteDatabase('studentdatabase.db') 
+from peewee import AutoField, ForeignKeyField, CharField
 
 class Staff(User):
-    staffID = AutoField(null = False)
-    userID = ForeignKeyField(User, to_field="userID", null = False)
-    fname = CharField(null = False)
-    mname = CharField()
-    lname = CharField(null = False)
-    email = CharField(unique = True)
+    staffID = AutoField(primary_key=True)
+    user = ForeignKeyField(User, to_field="userID", backref='staff')
+    fname = CharField()
+    mname = CharField(null=True)  # Assuming middle name can be null
+    lname = CharField()
+    email = CharField(unique=True)
     password = CharField()
     role = CharField()
+
+# Assuming 'db' is the instance of SqliteDatabase
+db.create_tables([Staff], safe=True)
