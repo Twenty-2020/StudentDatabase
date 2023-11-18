0aa83ba2beec97b844dce5dd9ec32fdc7cf01e87
Aciel <106684053+Aciel07@users.noreply.github.com>
2023-11-18 22:13:51 +0800

    Update Student.py

--- src/Student.py
+++ src/Student.py
@@ -1,19 +1,20 @@
 from User import User
 from BaseModel import BaseModel
-from peewee import *
+from peewee import AutoField, ForeignKeyField, CharField, IntegerField
 
-db = SqliteDatabase('studentdatabase.db') 
-
-class Student(User):
-    studentID = AutoField(null = False)
-    userID = ForeignKeyField(User, to_field="userID", null = False)
-    fname = CharField(null = False)
-    mname = CharField()
-    lname = CharField(null = False)
-    email = CharField(unique = True)
+class Student(BaseModel):
+    studentID = AutoField(primary_key=True)
+    user = ForeignKeyField(User, to_field="userID", backref='students')
+    fname = CharField()
+    mname = CharField(null=True)  # Assuming middle name can be null
+    lname = CharField()
+    email = CharField(unique=True)
     password = CharField()
     role = CharField()
     uploadeddocs = CharField()
     grades = IntegerField()
     enrolledSections = CharField()
 
+# AutoField(primary_key=True): Automatically increments for each new record.
+# ForeignKeyField(User, to_field="userID", backref='students'): Creates a foreign key relationship with the User model.
+# CharField(unique=True): Ensures uniqueness of the email.
