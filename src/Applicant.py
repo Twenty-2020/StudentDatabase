a3ac172024921e855593d422a89602a6f45b4542
Aciel <106684053+Aciel07@users.noreply.github.com>
2023-11-18 22:00:53 +0800

    Update Applicant.py

--- src/Applicant.py
+++ src/Applicant.py
@@ -1,15 +1,17 @@
-from User import User
-from BaseModel import BaseModel
-from peewee import *
-
-db = SqliteDatabase('studentdatabase.db') 
+from peewee import AutoField, CharField
+from User import User  
+from BaseModel import BaseModel  
 
 class Applicant(User):
-    applicantID = AutoField(null = False)
-    fname = CharField(null = False)
+    applicantID = AutoField(null=False)
+    fname = CharField(null=False)
     mname = CharField()
-    lname = CharField(null = False)
-    email = CharField(unique = True)
+    lname = CharField(null=False)
+    email = CharField(unique=True)
     uploadeddocs = CharField()
     role = CharField()
+
+    class Meta:
+        table_name = 'applicant'
+
 
