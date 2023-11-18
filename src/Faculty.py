a8caf2f2deb07aeefa4931e247ce1dcfcc79422c
Aciel <106684053+Aciel07@users.noreply.github.com>
2023-11-18 22:05:17 +0800

    Update Faculty.py

--- src/Faculty.py
+++ src/Faculty.py
@@ -1,16 +1,17 @@
 from User import User
 from BaseModel import BaseModel
-from peewee import *
-
-db = SqliteDatabase('studentdatabase.db') 
+from peewee import AutoField, ForeignKeyField, CharField
 
 class Faculty(User):
-    facultyID = AutoField(null = False)
-    userID = ForeignKeyField(User, to_field="userID", null = False)
-    fname = CharField(null = False)
-    mname = CharField()
-    lname = CharField(null = False)
-    email = CharField(unique = True)
+    facultyID = AutoField(primary_key=True)
+    user = ForeignKeyField(User, to_field="userID", backref='faculties')
+    fname = CharField()
+    mname = CharField(null=True)  # Assuming middle name can be null
+    lname = CharField()
+    email = CharField(unique=True)
     password = CharField()
     role = CharField()
     assignedSections = CharField()
+
+# Assuming 'db' is the instance of SqliteDatabase
+db.create_tables([Faculty], safe=True)
