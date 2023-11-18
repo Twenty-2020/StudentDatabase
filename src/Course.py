0bb135a974cf3fa5e413708d45a7195b049c633f
Aciel <106684053+Aciel07@users.noreply.github.com>
2023-11-18 22:01:24 +0800

    Update Course.py

--- src/Course.py
+++ src/Course.py
@@ -1,8 +1,12 @@
 from BaseModel import BaseModel
-from peewee import *
+from peewee import Model, SqliteDatabase, AutoField, CharField
 
-db = SqliteDatabase('studentdatabase.db') 
+db = SqliteDatabase('studentdatabase.db')
+
+class BaseModel(Model):
+    class Meta:
+        database = db
 
 class Course(BaseModel):
-    courseID = AutoField(null = False)
-    coursename = CharField(null = False)
+    courseID = AutoField(null=False)
+    coursename = CharField(null=False)
