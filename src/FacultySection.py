d1f642f5062d22cc6ac0d3f9b2119125874935f1
Aciel <106684053+Aciel07@users.noreply.github.com>
2023-11-18 22:05:43 +0800

    Update FacultySection.py

--- src/FacultySection.py
+++ src/FacultySection.py
@@ -1,10 +1,13 @@
 from BaseModel import BaseModel
-from peewee import *
 from Faculty import Faculty
+from Student import Student
 from Section import Section
-
-db = SqliteDatabase('studentdatabase.db') 
+from peewee import ForeignKeyField
 
 class FacultySection(BaseModel):
-    facultyID = ForeignKeyField(Faculty, to_field="facultyID")
-    sectionID = ForeignKeyField(Section, to_field="sectionID")
+    student = ForeignKeyField(Student, to_field="studentID", backref='faculty_sections')
+    section = ForeignKeyField(Section, to_field="sectionID", backref='faculty_sections')
+
+# ForeignKeyField(Student, to_field="studentID", backref='faculty_sections')`: Creates a foreign key relationship with the Student model.
+# ForeignKeyField(Section, to_field="sectionID", backref='faculty_sections')`: Creates a foreign key relationship with the Section model.
+# backref`: Creates a reverse relation in the Student and Section models so that you can access the associated faculty sections from a student or section instance.
