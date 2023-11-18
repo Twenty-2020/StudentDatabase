22eb653f584557d767dd97c079fd9b7d6a0f37f8
Arjen Catherine <144413569+ACPMolina@users.noreply.github.com>
2023-11-18 16:55:00 +0800

    Update Course.py

--- src/Course.py
+++ src/Course.py
@@ -4,5 +4,5 @@ from peewee import *
 db = SqliteDatabase('studentdatabase.db') 
 
 class Course(BaseModel):
-    courseID = AutoField()
-    coursename = CharField()
+    courseID = AutoField(null = False))
+    coursename = CharField(null = False))
