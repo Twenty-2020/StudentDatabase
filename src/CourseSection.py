66f35c073707ed3902b14913960cea0c597f753f
Aciel <106684053+Aciel07@users.noreply.github.com>
2023-11-18 22:01:39 +0800

    Update CourseSection.py

--- src/CourseSection.py
+++ src/CourseSection.py
@@ -1,9 +1,13 @@
 from BaseModel import BaseModel
-from peewee import *
+from peewee import Model, SqliteDatabase, ForeignKeyField
 from Course import Course
 from Section import Section
 
-db = SqliteDatabase('studentdatabase.db') 
+db = SqliteDatabase('studentdatabase.db')
+
+class BaseModel(Model):
+    class Meta:
+        database = db
 
 class CourseSection(BaseModel):
     courseID = ForeignKeyField(Course, to_field="courseID")
